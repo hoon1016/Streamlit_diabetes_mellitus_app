747502aa5cd3e9810b31536db6198827a677635d
hoon1016 <wjsrms76@gmail.com>
2021-03-12 16:40:28 +0900

    당뇨병 예측 앱

--- ML_app.py
+++ ML_app.py
@@ -18,32 +18,35 @@ import os
 from eda_app import run_eda_app
 import h5py
 from tensorflow.keras.callbacks import ModelCheckpoint,CSVLogger
-
+import joblib
 
 
 def run_ML_app():
-    st.subheader('Maching Learnig')
+   model = joblib.load('data/best_model.pkl')
+   df = pd.read_csv('data/diabetes.csv')
+   st.dataframe(df)
 
-    Pn = st.number_input("임신 횟수",min_value=0)
+   new_data = np.array([3,88,58,11,54,24,0.26,22])
+   new_data - new_data.reshape(1,-1)
+   print(new_data)
+
+   st.write(model.predict(new_data))
+   
+   
+    # st.subheader('Maching Learnig')
+
+    # Pn = st.number_input("임신 횟수",min_value=0)
     
-    Gc = st.number_input('포도당',min_value=0)
+    # Gc = st.number_input('포도당',min_value=0)
 
-    Bp = st.number_input('이완기 혈압',min_value=0)
+    # Bp = st.number_input('이완기 혈압',min_value=0)
 
-    stk = st.number_input('피부 주름 두께',min_value=0)
+    # stk = st.number_input('피부 주름 두께',min_value=0)
 
-    ins	= st.number_input("혈청 인슐린",min_value=0)
+    # ins	= st.number_input("혈청 인슐린",min_value=0)
     
-    BM = st.number_input('체중',min_value=0)
+    # BM = st.number_input('체중',min_value=0)
 
-    Dpe = st.number_input('당뇨병 혈통',min_value=0)
+    # Dpe = st.number_input('당뇨병 혈통',min_value=0)
 
-    age = st.number_input('나이',min_value=0,max_value=120)
-
-
-
-
-
-
-if __name__=='__main__':
-    main()
+    # age = st.number_input('나이',min_value=0,max_value=120)
